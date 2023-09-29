90c2968aebc8cccff923ef51b5ef241ef3a7ea6b
komangtio049 <127291407+komangtio049@users.noreply.github.com>
2023-09-29 20:24:43 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        lebarbgt: '.5em',
+        lebaranlgi: '.6em'
+      }
+    },
+  },
+  plugins: [],
+}
